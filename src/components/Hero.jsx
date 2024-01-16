fdd434c96650fec0ee92c6a16fce790dd86665f2
pedrolucas12 <pedrolucassantana@gmail.com>
2024-01-16 18:30:22 -0300

    Add Pedro Santana's resume and update download link
    This commit adds Pedro Santana's resume to the public directory and updates the download link in the Hero component.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -84,7 +84,7 @@ const Hero = () => {
           </div>
 
           <div className="w-full flex justify-start mt-20 lg:mt-[75px]">
-            <a href="/myresume.pdf" download>
+            <a href="/ResumePedroSantana.pdf" download>
               <div
                 className="border-violet-400 text-white 
                hover:text-violet-400 hover:border-violet-500 hover:text-white/90
